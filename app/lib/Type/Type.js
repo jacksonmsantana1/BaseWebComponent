4799e850238db224b17fdf92f2b696cf23a1ffbf
Jackson Martorano Sant'Ana <jackson.m.santana1@gmail.com>
2016-03-06 10:57:11 -0300

    Fixed the eslint errors from Type.js

--- app/lib/Type/Type.js
+++ app/lib/Type/Type.js
@@ -1,11 +1,13 @@
 import R from 'ramda';
 
-let checkType = R.curry(function(typeDef, actualType) {
+const checkType = R.curry((typeDef, actualType) => {
   if (R.is(typeDef, actualType)) {
     return actualType;
-  } else {
-    throw new TypeError('Type mismatch.Expected [' + typeDef + '] but found [' + typeof actualType + ']');
   }
+
+  throw new TypeError(`Type mismatch.Expected [${typeDef}]
+                        but found [typeof ${actualType}]`);
 });
 
-export default checkType;
+export
+default checkType;
