446f5f16362652adfd23cd019c4640792fda8191
Jackson Martorano Sant'Ana <jackson.m.santana1@gmail.com>
2016-03-29 10:36:20 -0300

    Created the pw-user-info

---
+++ app/components/pw-user-info/index.js
@@ -0,0 +1,1 @@
+export default './pw-user-info';
---
+++ app/components/pw-user-info/pw-user-info.test.js
@@ -0,0 +1,16 @@
+import expect from 'expect.js';
+import PwUserInfo from './pw-user-info.js';
+
+describe('pw-user-info', () => {
+  beforeEach(() => {
+
+  });
+
+  afterEach(() => {
+
+  });
+
+  it('Test should ...', () => {
+
+  });
+});
