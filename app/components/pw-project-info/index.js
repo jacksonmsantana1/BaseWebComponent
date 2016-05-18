317733d41dc93b146bb50691873e7347e6ceaad6
Jackson Martorano Sant'Ana <jackson.m.santana1@gmail.com>
2016-05-18 12:00:22 -0300

    Create the files

---
+++ app/components/pw-project-info/index.js
@@ -0,0 +1,1 @@
+export default './pw-project-info';
---
+++ app/components/pw-project-info/pw-project-info.js
@@ -0,0 +1,24 @@
+/**
+ * Component that will be the interface between the front and the backend
+ */
+
+//import Request from '../../lib/Request/Request.js';
+//import Logger from '../../lib/Logger/Logger.js';
+import R from 'ramda';
+
+//const contain = R.indexOf;
+//const get = R.prop;
+
+class PwProjectInfo extends HTMLElement {
+  createdCallback() {}
+
+  detachedCallback() {}
+
+  attachedCallback() {}
+
+  attributeChangedCallback() {}
+}
+
+document.registerElement('pw-project-info', PwProjectInfo);
+export
+default PwProjectInfo;
