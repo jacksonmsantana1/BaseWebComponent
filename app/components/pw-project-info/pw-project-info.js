24ce882eb1c5b2726c80027d8393ccb748ab89d9
Jackson Martorano Sant'Ana <jackson.m.santana1@gmail.com>
2016-05-23 17:48:03 -0300

    like() method almost done

--- app/components/pw-project-info/pw-project-info.js
+++ app/components/pw-project-info/pw-project-info.js
@@ -4,12 +4,24 @@
 
 //import Request from '../../lib/Request/Request.js';
 //import Logger from '../../lib/Logger/Logger.js';
-//import R from 'ramda';
+import R from 'ramda';
+import Request from './../../lib/Request/Request';
+import Token from './../../lib/Token/Token';
 
 //const contain = R.indexOf;
 //const get = R.prop;
+const isNil = R.isNil;
+
+//
+const putJSON = (url, data) =>
+  Request.putJSON(url, data, Token.getUserToken());
 
 class PwProjectInfo extends HTMLElement {
+
+  constructor() {
+    super();
+    this._project = {};
+  }
 
   /*******************Inherited Methods*********************/
 
@@ -26,7 +38,33 @@ class PwProjectInfo extends HTMLElement {
   /************************Methods***************************/
 
   liked(evt) {
-    console.log(evt.target);
+    let url;
+    let data;
+
+    return new Promise((resolve, reject) => {
+      if (!evt || evt.type !== 'like') {
+        reject(new Error('Invalid Event'));
+      } else if (isNil(evt.detail)) {
+        reject(new Error('Missing Event Detail'));
+      } else if (evt.detail.projectId !== this._project.id) {
+        reject(new Error('Invalid Event Detail'));
+      }
+
+      url = '/projects/' + evt.detail.projectId + '/liked';
+      data = { projectId: evt.detail.projectId }; //FIXME
+
+      putJSON(url, data)
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
+    });
+  }
+
+  get project() {
+    return this._project;
+  }
+
+  set project(project) {
+    this._project = project;
   }
 }
 
--- app/components/pw-project-info/pw-project-info.test.js
+++ app/components/pw-project-info/pw-project-info.test.js
@@ -1,20 +1,32 @@
 import expect from 'expect.js';
 import Logger from '../../lib/Logger/Logger.js';
+import Token from '../../lib/Token/Token';
 
 describe('pw-project-info', () => {
   describe('Events => ', () => {
     let component;
     let pwProjectInfo;
+    let project;
     let xhr;
     let requests;
+    let token;
 
     beforeEach(() => {
       xhr = sinon.useFakeXMLHttpRequest();
       requests = [];
+      project = {
+        id: '123456',
+        name: 'Project1',
+      };
 
       component = document.createElement('pw-project-info');
       document.body.appendChild(component);
+
       pwProjectInfo = document.getElementsByTagName('pw-project-info')[0];
+      pwProjectInfo.project = project;
+
+      token = 'TOKEN';
+      Token.setUserToken(token);
 
       xhr.onCreate = (xhr) => {
         requests.push(xhr);
@@ -25,7 +37,66 @@ describe('pw-project-info', () => {
       sinon.restore();
     });
 
-    it('Should be listening for the like event', () => {
+    describe('Attributes', () => {
+      it('Should contain the project attribute', () => {
+        expect(pwProjectInfo.project).to.be.equal(project);
+      });
+    });
+
+    describe('liked() => ', () => {
+      it('Should return an promise', () => {
+        expect(pwProjectInfo.liked('').constructor.name).to.be.equal('Promise');
+      });
+
+      it('Should receive an Event as argument', (done) => {
+        const evt = new CustomEvent('not-like-event');
+
+        pwProjectInfo.liked(evt).catch((err) => {
+          expect(err.message).to.be.equal('Invalid Event');
+          done();
+        });
+      });
+
+      it('Should the event received to contain the project id liked', (done) => {
+        const evt = new CustomEvent('like');
+
+        pwProjectInfo.liked(evt).catch((err) => {
+          expect(err.message).to.be.equal('Missing Event Detail');
+          done();
+        });
+      });
+
+      it('Should return an error if the event gives a different project id', (done) => {
+        const evt = new CustomEvent('like', { detail: { projectId: 'ANUS' } });
+
+        pwProjectInfo.liked(evt).catch((err) => {
+          expect(err.message).to.be.equal('Invalid Event Detail');
+          done();
+        });
+      });
+
+      it('Should send a PUT request to the endpoint /projects/{id}/liked', (done) => {
+        const evt = new CustomEvent('like', { detail: { projectId: '123456' } });
+
+        pwProjectInfo.liked(evt)
+          .then((res) => {
+            expect(JSON.parse(res.body)).to.be.equal(true);
+            done();
+          })
+          .catch((err) => {
+            done(err);
+          });
+
+        expect(requests[0].url).to.be.equal('http://localhost:3000/projects/123456/liked');
+        expect(requests[0].requestHeaders.authorization).to.be.equal('TOKEN');
+        expect(requests[0].method).to.be.equal('PUT');
+        requests[0].respond(200, {}, 'true');
+      });
+
+      it('Should send a PUT request to the endpoint /users/{id}/liked', (done) => {
+        //TODO Wait to make the User Service endpoint /users/{id}/liked
+        done();
+      });
     });
   });
 });
